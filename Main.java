3aa27fe9bbd493abcccf747680b11af81efa63f4
Elixir231 <49114523+Elixir231@users.noreply.github.com>
2019-03-31 10:25:09 +0200

    Add files via upload
    Update to the previous codes:
    Accepts user name
    More Options added

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 package com.company;
 
+import java.util.IllformedLocaleException;
 import java.util.Scanner;
 public class Main
 {
     public static void main (String [] args)
     {
         System.out.println("Welcome to Call care Online Customer Service!!!");
-        System.out.println("How can we help you?");
+        System.out.println("Kindly enter your name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("How can we help you today?  " + name);
         System.out.println("To change your preferred language! please Press 1");
+        System.out.println("To Make enquiries about your device, press 2");
 
         Scanner cus = new Scanner(System.in);
         int x = cus.nextInt();
@@ -30,7 +35,50 @@ public class Main
                     break;
             }
 
-        }else
+        }else if (x == 2){
+            System.out.println("To Check your line status press 1");
+            System.out.println("To Check your phone number press 2");
+            System.out.println("To Check your account balance 3");
+            Scanner stat = new Scanner(System.in);
+            int s = stat.nextInt();
+
+            if (s == 1){
+                System.out.println("Your line status Verification in process");
+                for (int i=0; i<=5; i++)
+                {
+                    System.out.println(".............");
+                }
+                System.out.println("Signal Check Okay");
+                for (int i=0; i<=5; i++)
+                {
+                    System.out.println(".............");
+                }
+                System.out.println("Call Connectivity Okay");
+                for (int i=0; i<=5; i++)
+                {
+                    System.out.println(".............");
+                }
+                System.out.println("Sms Check Okay");
+                for (int i=0; i<=5; i++)
+                {
+                    System.out.println(".............");
+                }
+                System.out.println("Status Okay");
+            }
+
+            if (s == 2){
+                System.out.println("Your Phone number is: ");
+                System.out.println("090761257389");
+            }
+            if (s == 3){
+                System.out.println("Your Current Account balance is:");
+                System.out.println("234.34$");
+            }
+            else {
+                System.out.println("error");
+            }
+        }
+        else
         {
             System.out.println("Invalid Selection");
         }
